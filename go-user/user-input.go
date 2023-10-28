f38ed04b8c8df1ac3f6282ddcc0573f726fd2259
aliha <alihajji315@gmail.com>
2023-10-28 13:28:21 +0100

    new tache

---
+++ go-user/user-input.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var fName string
+	fmt.Println("Enter First Name :")
+	fmt.Scanln(&fName)
+
+	var lName string
+	fmt.Println("Enter last Name :")
+	fmt.Scanln(&lName)
+}
